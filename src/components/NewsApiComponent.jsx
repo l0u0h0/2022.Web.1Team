17a18573855ac8b8edf72e42716f7dc9658834ce
l0u0h0 <l0u0h0@gmail.com>
2022-05-24 23:19:10 +0900

    News_api_fetch

---
+++ src/components/NewsApiComponent.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NewsItem from "../js/NewsItem";
+
+export default function NewsApi() {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b3e1df4f86f4c4ebae8f5389c7d8ba6"
+        );
+        setArticles(response.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchData(false);
+  }, []);
+  if (loading) {
+    return <div>로딩중.....</div>;
+  }
+  if (!articles) {
+    return null;
+  }
+  return (
+    <div className="container_news">
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </div>
+  );
+}
---
+++ src/js/NewsItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const NewsItem = ({ article }) => {
+  const { title, description, url, urlToImage } = article;
+  return (
+    <div className="list">
+      {urlToImage && (
+        <div className="thumnail">
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img src={urlToImage} alt="thumnail" />
+          </a>
+        </div>
+      )}
+      <div className="contents">
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+export default NewsItem;
