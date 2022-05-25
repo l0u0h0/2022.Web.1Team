f59b765951997342964455671de8b91017497bf0
l0u0h0 <l0u0h0@gmail.com>
2022-05-25 09:22:33 +0900

    error_fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import NewsApi from "./components/NewsApiComponent";
+import React from "react";
 
 function App() {
   return (
-    <>
+    <div style={{ width: "70%", height: "400px" }}>
       <NewsApi />
-    </>
+    </div>
   );
 }
 
--- src/components/NewsApiComponent.jsx
+++ src/components/NewsApiComponent.jsx
@@ -13,6 +13,8 @@ export default function NewsApi() {
         const response = await axios.get(
           "https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b3e1df4f86f4c4ebae8f5389c7d8ba6"
         );
+        const evcs = response.data.articles.title;
+        console.log(evcs);
         setArticles(response.data.articles);
       } catch (error) {
         console.log(error);
