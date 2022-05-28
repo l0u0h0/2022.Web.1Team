9bd80750dcbaa06e20e590dd5ee4fc2bc94bced0
l0u0h0 <l0u0h0@gmail.com>
2022-05-28 16:19:00 +0900

    전기차 issue api

--- src/components/NewsApiComponent.jsx
+++ src/components/NewsApiComponent.jsx
@@ -5,16 +5,15 @@ import NewsItem from "../js/NewsItem";
 export default function NewsApi() {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [list, setList] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
         const response = await axios.get(
-          "https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b3e1df4f86f4c4ebae8f5389c7d8ba6"
+          "https://newsapi.org/v2/everything?q=전기차&top-headlines?country=kr&apiKey=6b3e1df4f86f4c4ebae8f5389c7d8ba6"
         );
-        const evcs = response.data.articles.title;
-        console.log(evcs);
         setArticles(response.data.articles);
       } catch (error) {
         console.log(error);
