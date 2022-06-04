64441c08190d29ed1131866a568308e308c94445
l0u0h0 <l0u0h0@gmail.com>
2022-06-04 16:41:55 +0900

    ChargeHistory_add

--- src/components/HistoryComponent.jsx
+++ src/components/HistoryComponent.jsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function History(props) {
+export default function ChargeHistory(props) {
   const month = props.date + 1;
+  const [list, setList] = useState({ list: [0, 0, 0] });
+  useEffect(() => {
+    setList({ list: [month - 2, month - 1, month] });
+  }, [month]);
+  console.log(list);
+  list.list.map((month) => {
+    return console.log(month);
+  });
   return (
     <div className="container-history">
-      <div className="history">
-        <h1>{month - 2}월 충전 내역</h1>
-        <hr />
-        <div className="history-box">
-          <p>충전소 명칭</p>
-          <p>충전량</p>
-          <p>충전 비용</p>
-        </div>
-        <div className="history-box">
-          <p>충전소 명칭</p>
-          <p>충전량</p>
-          <p>충전 비용</p>
-        </div>
-        <div className="history-box">
-          <p>충전소 명칭</p>
-          <p>충전량</p>
-          <p>충전 비용</p>
-        </div>
+      {list.list.map((month) => (
+        <History month={month} />
+      ))}
+    </div>
+  );
+}
+
+function History(props) {
+  const month = props.month;
+  return (
+    <div className="history">
+      <h1>{month}월 충전 내역</h1>
+      <hr />
+      <div className="history-box">
+        <p>충전소 명칭</p>
+        <p>충전량</p>
+        <p>충전 비용</p>
+      </div>
+      <div className="history-box">
+        <p>충전소 명칭</p>
+        <p>충전량</p>
+        <p>충전 비용</p>
+      </div>
+      <div className="history-box">
+        <p>충전소 명칭</p>
+        <p>충전량</p>
+        <p>충전 비용</p>
       </div>
     </div>
   );
