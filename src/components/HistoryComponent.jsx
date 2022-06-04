41af93ceee89346b9899ce9db47525d093497218
l0u0h0 <l0u0h0@gmail.com>
2022-06-04 15:43:01 +0900

    history_add

--- src/components/HistoryComponent.jsx
+++ src/components/HistoryComponent.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function History(date) {
+  const month = date.date + 1;
   return (
     <div>
-      <p>{date}월 충전 내역</p>
+      <p>{month}월 충전 내역</p>
     </div>
   );
 }
