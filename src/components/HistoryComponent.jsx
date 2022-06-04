bf783f02be1f022c35a15d5ff2a6b53f524a8856
l0u0h0 <l0u0h0@gmail.com>
2022-06-04 16:22:54 +0900

    history-box_hr_add,margin_update

--- src/components/HistoryComponent.jsx
+++ src/components/HistoryComponent.jsx
@@ -6,6 +6,17 @@ export default function History(props) {
     <div className="container-history">
       <div className="history">
         <h1>{month - 2}월 충전 내역</h1>
+        <hr />
+        <div className="history-box">
+          <p>충전소 명칭</p>
+          <p>충전량</p>
+          <p>충전 비용</p>
+        </div>
+        <div className="history-box">
+          <p>충전소 명칭</p>
+          <p>충전량</p>
+          <p>충전 비용</p>
+        </div>
         <div className="history-box">
           <p>충전소 명칭</p>
           <p>충전량</p>
